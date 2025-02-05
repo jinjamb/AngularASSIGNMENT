cca47641f4c27bc2b2fd88f5aba599b1d2884647
Michel Buffa <michelbuffa@macbook-pro-de-michel-2.home>
2025-02-05 15:11:49 +0100

    ajout d'assignment

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,33 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RenduDirective } from '../shared/rendu.directive';
 import { NonRenduDirective } from '../shared/non-rendu.directive';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { Assignment } from './assignment.model';
 
 @Component({
   selector: 'app-assignments',
-  imports: [CommonModule, RenduDirective, NonRenduDirective],
+  providers: [provideNativeDateAdapter()],
+  imports: [CommonModule, RenduDirective, NonRenduDirective, 
+    MatButtonModule, MatFormFieldModule, MatInputModule, 
+    FormsModule, MatDatepickerModule],
   templateUrl: './assignments.component.html',
   styleUrl: './assignments.component.css'
 })
-export class AssignmentsComponent {
+export class AssignmentsComponent implements OnInit {
   titre = 'Liste des assignments';
-  assignments = [
+  //ajoutActive = false;
+  // Pour le formulaire d'ajout
+  nomDevoir = "";
+  dateDeRendu:Date|null = null;
+
+  assignments:Assignment[] = [
     {
       nom: 'TP Angular',
-      dateDeRendu: '2024-3-17',
+      dateDeRendu: new Date('2024-3-17'),
       rendu: false
     },
     {
       nom: 'TP Mr Galli',
-      dateDeRendu: '2023-12-17',
+      dateDeRendu: new Date('2023-12-17'),
       rendu: true
     },
     {
       nom: 'Projet J2EE',
-      dateDeRendu: '2024-4-15',
+      dateDeRendu: new Date('2024-4-15'),
       rendu: false
     }
   ]
+
+  ngOnInit() {
+    console.log("ngOnInit appelé lors de l'instanciation du composant");
+    /*
+    // on veut passer la propriété ajoutActive à true au bout de 3 secondes
+    setTimeout(() => {
+      this.ajoutActive = true;
+    }, 3000);
+    */
+  }
+  onSubmit(event:any) {
+    console.log(`On a soumis le formulaire nom = ${this.nomDevoir}, 
+      dateDeRendu = ${this.dateDeRendu}`);
+
+      // On ne crée un nouvel assignment que si le formulaire est valide
+      // c'est-à-dire si le nom du devoir n'est pas vide et si la date de rendu est bien définie
+      if(this.nomDevoir == "" || this.dateDeRendu == null) {
+        console.log("Formulaire invalide");
+        return;
+      }
+
+      let nouvelAssignment = new Assignment();
+      nouvelAssignment.nom = this.nomDevoir;
+      nouvelAssignment.dateDeRendu = this.dateDeRendu;
+      nouvelAssignment.rendu = false;
+
+      // On ajoute le nouvel assignment au tableau
+      this.assignments.push(nouvelAssignment);
+  }
 
   getColor(a:any):string {
     if(a.rendu) return 'green';
