c09f4ef9734144cd25e975d7b61955272cec6f01
Michel Buffa <michelbuffa@luci153020.unice.fr>
2025-02-12 18:51:09 +0100

    avec pagination

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -14,25 +14,6 @@ import { AssignmentDetailComponent } from './assignment-detail/assignment-detail
 import { AssignmentsService } from '../shared/assignments.service';
 import { RouterLink } from '@angular/router';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-assignments',
   imports: [CommonModule, RenduDirective, NonRenduDirective,
@@ -60,8 +41,6 @@ export class AssignmentsComponent implements OnInit {
   nextPage = 2;
   // Pour la data table angular
   displayedColumns: string[] = ['nom', 'dateDeRendu', 'rendu', '_id'];
-  dataSource = this.assignments;
-
 
   // Attention, pour l'injection de service, mettre en private !!! Sinon
   // ça ne marche pas
